832e058a9fea5686a3d3b30b223f197e33626672
Ø <79362520+legitnull@users.noreply.github.com>
2021-03-30 01:07:41 -0600

    Add files via upload

--- demonic.py
+++ demonic.py
@@ -1,21 +1,3 @@
-#this project is for a small community of discord bot enthusiasts and has been highly suggested for me to make off of an earlier prototype
-title = """
-    ..                      .oo.                        .;,                    ..       .:,           ..                    ..     .'.
-   .x0c                     .dN0c.         .,,.        .dWK;           ...  'cdOOl.  .:dko,         .cO0l.                .:kx.   'kKo.    .,.
-   .xW0'        ....'..      ;KKOk:. .;'   :XNk'       .OWNx.         'kOccoo:;kW0:;dxo;.':'       .oNNx.        .dk,  .;dkx:.   .dNWO.   cKNO'
-.,,'oNNd;:clodxkkkk0XXo      .xXl:k0dkO,   'OWN0;      :XNW0,        .dN0d:.  .xWXOo;.  .oNXx,     ;KWO'         :XNklxOxc.      :XNNNd. ;KX0:
-:OK0KNWXOxdlc:,'...oNXc       ,00,.lKX:     oNNNK:   .cONXNXc       ,xXK:   .:xXNd.      .oXWXx,   lNNo        .;xNNKd:'        .kW0dKNd'dWOc.
- ...,OWK:         ,0Wx.   .....;00; ',      ;KNNNKc .dNNOo0Wd.    ;doxNk..:dxdkNK;        .oNNNXd'.lNNl    .,lxOxOXWd.          :XWo.,ONKKWk;.
-     oNNl        'ONx. ,oOOOOOOkKWKc.     ...xNK0NXloXWX:.xWO'  'xx,.oN0xkd:..xWd.         .xNNXNKoxNNl .cxOOd:. ;KWd.          oWX:  .dNNWx'.    ''
-     ;XWx.      ;0Nd..d0x;'..';:ld00,    'kl cNKldNNNNXo  cNX: .ONd:dKNk;.   :X0,           '0WOoONNNNx.;xo,.    ;KWd.         .xW0'   .lKKc.    .kXd.
-     .OW0'    .lKKl.'kO,          ..    .x0' '0Nl.oKKd:.  ,0Wd..oOOxxXX:    'OXc             cXNc'dNNNK,         ;XWd.     .:x:.kWO.     ..       ;KNl
-     .dWX:   ,kXk' .oNk'                l0:  .xWx. ..     .xW0'  .  ,0K,   .dNo.             .xWO'.lKNNd.        :XWd.   ,d0kl'.OWO.              '0Wd
-      cXWd.,xKO;    .c0Kd,            .dO;   .dW0,         cNNc     ,KK,  .oXd.               :XNc  :KWX:        :XWd.'ckOo'   .kW0'             .oNX:
-      ,0WKO0x;.        .l0Xkl;'.....':dko. kX00NX;         'OWk.    ;KK, .dKl.                .kWO.  cXWO'       :XWX00kc.      lNNl            ,kX0:
-;xkdllxXWNx.            .:x0XXK00000xc.    .l0NNX:          oNXc    ,KNockk,                   cXNc  .dNXo.   .;lOKOo;.         .xNKc.     ..'cxKOc.
-,xO0Oxoll;.                ..,,,,,'.         .d0d.          '0WO'   .dXKkc.                    .OWx.  .''..;okOkd:.              .cOKOxooodddddc'
-  ..                                           .             ,ol'     ..                        .;'       ;xo:'.                    .,,;,,...
-""" # title=gangshit2.flt
-
-# imp(dataIn='./config,js')
+title = open('./modules/title.txt').read()
+import os, sys, discord
 print(title)
